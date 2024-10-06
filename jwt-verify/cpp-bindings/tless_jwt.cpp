e7b39dc9d010dcbe8812b29953b341f01e6caed3
Carlos Segarra <carlos@carlossegarra.com>
2024-10-06 12:13:36 +0000

    cpp-bindings: add wrapper

--- jwt-verify/cpp-bindings/tless_jwt.cpp
+++ jwt-verify/cpp-bindings/tless_jwt.cpp
@@ -8,4 +8,9 @@ bool verify(const std::string& jwt)
 {
     return verify_jwt(jwt.c_str());
 }
+
+bool checkProperty(const std::string& jwt, const std::string& property, char* expectedValue)
+{
+    return check_property(jwt.c_str(), property.c_str(), expectedValue);
+}
 }
--- jwt-verify/cpp-bindings/tless_jwt.h
+++ jwt-verify/cpp-bindings/tless_jwt.h
@@ -4,8 +4,10 @@
 
 extern "C" {
 bool verify_jwt(const char* jwt);
+bool check_property(const char* jwt, const char* property, const char* exp_value);
 }
 
 namespace tless::jwt {
 bool verify(const std::string& jwt);
+bool checkProperty(const std::string& jwt, const std::string& property, char* expectedValue);
 }
